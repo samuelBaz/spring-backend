6e1447764b72473ca565286d27d66b37747fbf24
bazoaltoSamuel <bazoaltoSamuel33@outlook.com>
2021-08-30 12:50:12 -0400

    add a new model

---
+++ src/main/java/com/sales/market/model/Buy.java
@@ -0,0 +1,24 @@
+/**
+ * @author: Samuel Bazoalto
+ */
+package com.sales.market.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+public class Buy extends ModelBase {
+
+    @Column(precision = 10, scale = 5)
+    private BigDecimal value;
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
