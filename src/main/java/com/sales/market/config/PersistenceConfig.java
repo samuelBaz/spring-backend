40f2c38029688ec46e97d95ab3b272ffc11f061b
bazoaltoSamuel <bazoaltoSamuel33@outlook.com>
2021-09-01 14:03:38 -0400

    add configurations

---
+++ src/main/java/com/sales/market/config/PersistenceConfig.java
@@ -0,0 +1,17 @@
+/**
+ * @author: Samuel Bazoalto
+ */
+package com.sales.market.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories
+@EnableJpaAuditing
+public class PersistenceConfig {
+
+}
