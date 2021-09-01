da40dca1e663576d92af02754f637090ca96c3f1
bazoaltoSamuel <bazoaltoSamuel33@outlook.com>
2021-09-01 14:04:01 -0400

    add exceptions

---
+++ src/main/java/com/sales/market/exception/ValidationException.java
@@ -0,0 +1,31 @@
+/**
+ * @author: Samuel Bazoalto
+ */
+package com.sales.market.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ValidationException extends RuntimeException {
+    public ValidationException() {
+        super();
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ValidationException(String message, Throwable cause, boolean enableSuppression, boolean
+            writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
